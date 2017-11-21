09a57648052e3eec75881f0f8e1e55fceb467d4c
Sue <suziejanie692@gmail.com>
2017-11-21 22:26:00 +0300

    start working on the API

--- flask/user_register.py
+++ flask/user_register.py
@@ -1,11 +1,29 @@
-from flask import Flask
+#!flask/bin/python
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+users = [
+    {
+        'fname': u'sue',
+        'lname': u'smith',
+        'uname': u'sue', 
+        'email': u'sue@outlook.com',
+        'pwd': 'sue'
 
+    },
+    {
+        'fname': u'sam',
+        'lname': u'smith',
+        'uname': u'sam', 
+        'email': u'sam@outlook.com',
+        'pwd': 'sam'
+    }
+]
+
+@app.route('/brightEvents/api/auth/register', methods=['GET'])
+def get_users():
+    return jsonify({'users': users})
 
 if __name__ == '__main__':
     app.run(debug=True)
