b26a18fd1a9e078ed339851115c541fbecd10832
Sue <suziejanie692@gmail.com>
2017-11-22 03:19:40 +0300

    test case for the events

---
+++ test_events.py
@@ -0,0 +1,12 @@
+import unittest
+
+from events import event
+
+class eventsCreation(unittest.TestCase):
+    def setUp(self):
+        eve=event(1, 2, 3, 4)
+
+    def test_typeOfFields(self):
+        eve=event(1, 2, 3, 4)
+        result=eve.add_event(1, 2, 3, 4)
+        self.assertEquals("Invalid input. Enter character elements", result)
