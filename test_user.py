4c7df223e9dd69c1ec8d62d76adb2d451ded3886
Sue <suziejanie692@gmail.com>
2017-11-22 16:52:20 +0300

    add more test cases

--- test_user.py
+++ test_user.py
@@ -14,13 +14,13 @@ class userRegistrationDetailsTestCase(unittest.TestCase):
         result=self.myUser.add_user("", "","" , "", "")
         self.assertEquals("Kindly fill out all the form fields", result)
     def test_lengthPasswordLessThanEightCharacters(self):
-        result=self.myUser.add_user("sue", "smith","sue" , "sue@oulook.com", "vg")
-        self.assertEquals("Kindly fill out all the form fields", result)
+        result=self.myUser.add_user("sue", "smith","sue" , "sueoulookcom", "vg")
+        self.assertEquals("Password length too small", result)
 
 #class for checking the user details on the login page
 class userLoginTestCase(unittest.TestCase):
     def setUp(self):
-        self.myUser=user(1, 2, 3, 4, 2)
+        self.myUser=login()
     def test_loginDetailIfTheyExist(self):
         result=self.myUser.login_user("", "joe")
         self.assertEquals("Kindly fill out all the form fields", result)
--- users.py
+++ users.py
@@ -1,15 +1,13 @@
 class user(object):
-
+    #details list will be used to store a list of dictionaries for user accounts
+    details=[]
     def __init__(self,fname, lname,uname,email,pwd):
-        
         self.fname = fname
         self.lname =lname
         self.uname =uname
         self.email =email
         self.pwd = pwd
 
-        details={}
-        loginDetails={}
         
 
     def add_user(self,fname, lname,uname,email,pwd):
@@ -19,28 +17,36 @@ class user(object):
         self.email =email
         self.pwd = pwd
         if fname and lname and uname and email and pwd:
-            if type(fname) or type(name) or type(uname) or type(email) !=str:
+            if type(fname) or type(name) or type(uname) !=str:
                 return "Invalid input. Enter character elements"
             
-            elif len(pwd)<8:
+            elif len(pwd) <= 7:
                 return("Password length too small")
             else:
                  #store details in a dictionary
-                self.details={'Firstname':fname,'Lastname':lname,'Username':uname,'Email':email, 'Password':pwd}
-                self.loginDetails[uname]=pwd
+                self.details.append=({'Firstname':fname,'Lastname':lname,'Username':uname,'Email':email, 'Password':pwd})
+            
         else:
             return "Kindly fill out all the form fields"
   
 class login(user):
+    details=user.details
+    
     def __init__(self):
         user.__init__(self)
     def login_user(self, uname, pwd):
         self.uname =uname
         self.pwd = pwd
-        if uname and pwd:
-            return "login successful"
-        else:
+        if not uname and pwd:
             return"Kindly fill out all the form fields"
+        for detail in details:
+            newUname= name.get('Username')
+            newPwd = name.get('pwd')
+            if newUname and newPwd:
+                return "login successful"
+            else:
+                return "username or Password incorrect"
+            
     def logout_user(self, uname):
         self.uname =uname
         session.delete()
