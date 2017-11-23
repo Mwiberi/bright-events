e091a07afcd15bec97a9481d63544111e77554e1
Sue <suziejanie692@gmail.com>
2017-11-23 11:36:19 +0300

    make changes to the api user side

--- app/users.py
+++ app/users.py
@@ -1,6 +1,11 @@
 
 #!flask/bin/python
 from flask import Flask, jsonify
+from flask_httpauth import HTTPBasicAuth
+#create an object of HTTPBasicAuth
+auth=HTTPBasicAuth()
+
+
 
 app = Flask(__name__)
 
@@ -22,7 +27,7 @@ users = [
     }
 ]
 
-#unction to create a new user
+#Function to create a new user
 @app.route('/brightEvents/api/v1/auth/register', methods=['POST'])
 def create_users():
     if not request.json or not 'uname' in request.json:
@@ -36,5 +41,28 @@ def create_users():
     }
     users.append(user)
     return jsonify({'user': user}), 201
+
+#Function to get the user login details and user login if they are correct
+@app.route('brightEvents/api/v1/auth/login', methods=['POST'])
+@auth.login_required
+@auth.get_LoginDetails
+def getLoginDetails(uname,pwd):
+user = [user for user in userss if user['uname'] == uname and user['pwd'] == pwd]
+ if len(user) == 0:
+        abort(401)
+else:
+    return jsonify({'user': user[0]})
+
+# make a better eror 401 response
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorizd!Incorrect username or password'}), 401)
+
+   
+
+
+
+
+ #The main method
 if __name__ == '__main__':
     app.run(debug=True)
