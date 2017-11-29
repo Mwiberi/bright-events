e57f9b28f8ffc4df0e3471c4b353d3f9cafb4203
Sue <suziejanie692@gmail.com>
2017-11-29 17:26:09 +0300

    add more tests to the user class

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,16 +5,16 @@ from users import Login
 
 class userRegistrationDetailsTestCase(unittest.TestCase):
     def setUp(self):
-        self.myUser=user(1, 2, 3, 4, 2)
+        self.myUser=User(1, 2, 3, 4, 2)
 
     def test_typeOfFields(self):
-        result=self.myUser.add_user(1, 2, 3, 4, 2)
+        result=self.myUser.create_user(1, 2, 3, 4, 2)
         self.assertEquals("Invalid input. Enter character elements", result)
     def test_emptyFields(self):
-        result=self.myUser.add_user("", "","" , "", "")
+        result=self.myUser.create_user("", "","" , "", "")
         self.assertEquals("Kindly fill out all the form fields", result)
     def test_lengthPasswordLessThanEightCharacters(self):
-        result=self.myUser.add_user("sue", "smith","sue" , "sueoulookcom", "vg")
+        result=self.myUser.create_user("sue", "smith","sue" , "sue@oulookcom", "vg")
         self.assertEquals("Password length too small", result)
 
 #class for checking the user details on the login page
